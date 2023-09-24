358d0037be49cb37b6007a19b879e2b476e1a199
jaime <jaimevaz75@gmail.com>
2023-09-24 20:06:44 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "Saiu a nova versão do Angular",
+    "description": "O Angular tornou-se uma das estruturas front-end mais populares para a criação de aplicativos Web dinâmicos.",
+    "photoCover":"https://vidafullstack.com.br/wp-content/uploads/2021/07/versao-12-angular-980x487.png"
+  },
+  {
+    "id": "2",
+    "title": "What’s New in Spring Boot 3 for Java Developers in 2023",
+    "description": "mais informações...",
+    "photoCover":"https://examples.javacodegeeks.com/wp-content/uploads/2023/01/Spring-Boot-3-Java-1024x576.jpg"
+  },
+  {
+    "id": "3",
+    "title": "Get your team trained on Jira with this resource.",
+    "description": "mais informações...",
+    "photoCover":"https://community.atlassian.com/t5/image/serverpage/image-id/7247i9A869F7319D8ABCB/image-dimensions/503x279?v=v2"
+  },
+  {
+    "id": "4",
+    "title": "Ubuntu 23.04 chegou: conheça 4 grandes novidades da atualização",
+    "description": "mais informações...",
+    "photoCover":"https://t.ctcdn.com.br/IQ2iVXtnjiXkC8scmEc1quSnMJE=/640x360/smart/i727184.png"
+  }
+];

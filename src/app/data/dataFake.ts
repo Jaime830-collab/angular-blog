64a34344a83e3e5cc8a6426e49756c17cad60917
jaime <jaimevaz75@gmail.com>
2024-04-21 10:56:37 -0300

    Atualizacao de dados

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,26 +1,26 @@
 export const dataFake = [
   {
     "id": "1",
-    "title": "Saiu a nova versão do Angular",
-    "description": "O Angular tornou-se uma das estruturas front-end mais populares para a criação de aplicativos Web dinâmicos.",
-    "photoCover":"https://vidafullstack.com.br/wp-content/uploads/2021/07/versao-12-angular-980x487.png"
+    "title": "Confira quais são as tendências para 2024.",
+    "description": "Inteligência Artificial e Automação, Segurança Cibernética Avançada, Sustentabilidade e Tecnologias Verdes, Realidade Virtual e Aumentada, Computação Quântica.",
+    "photoCover":"https://img.olhardigital.com.br/wp-content/uploads/2023/12/Tecnologia-2024.jpg"
   },
   {
     "id": "2",
-    "title": "What’s New in Spring Boot 3 for Java Developers in 2023",
-    "description": "mais informações...",
-    "photoCover":"https://examples.javacodegeeks.com/wp-content/uploads/2023/01/Spring-Boot-3-Java-1024x576.jpg"
+    "title": "Novidades no Angular 17",
+    "description":"A equipe do Angular chamou esta versão de Renascença do framework. Muitas pessoas acharam que seria um novo framework, talvez um Angular 3. Mas a verdade é que a versão 17 continua com grandes features, assim como as versões anteriores, focando principalmente em performance e Developer Experience.",
+    "photoCover":"https://miro.medium.com/v2/resize:fit:720/format:webp/0*KNqK1IFDqENcyGXS"
   },
   {
     "id": "3",
-    "title": "Get your team trained on Jira with this resource.",
-    "description": "mais informações...",
-    "photoCover":"https://community.atlassian.com/t5/image/serverpage/image-id/7247i9A869F7319D8ABCB/image-dimensions/503x279?v=v2"
+    "title": "OpenAI lança GPT-4 Turbo mais barato e com informações atuais",
+    "description": "1 - Peça ao GPT-4 para ler contratos e apontar as cláusulas com as quais você. 2 - Faça pesquisas para compreender melhor o perfil dos seus clientes. 3 - Considere o GPT-4 como um funcionário confiável. 4 - Treine o GPT-4 para aprender seu tom de voz. 5 - Conte com o GPT-4 para traçar suas diretrizes de Desenvolvimento Profissional.",
+    "photoCover":"https://img.olhardigital.com.br/wp-content/uploads/2023/03/gpt-4.jpg"
   },
   {
     "id": "4",
-    "title": "Ubuntu 23.04 chegou: conheça 4 grandes novidades da atualização",
-    "description": "mais informações...",
-    "photoCover":"https://t.ctcdn.com.br/IQ2iVXtnjiXkC8scmEc1quSnMJE=/640x360/smart/i727184.png"
+    "title": "O Ubuntu 23.04 está entre nós e estas são as principais novidades",
+    "description": "O Gnome 44 é o principal atrativo da distribuição. A nova versão do ambiente de desktop não traz nenhuma grande novidade. Em vez disso, há um conjunto de pequenas mudanças que o tornam interessante não só pelo visual, mas pelo aspecto funcional.",
+    "photoCover":"https://t.ctcdn.com.br/jN7VYsQA8z5_wX9dYZ8PEST4ZfQ=/1024x576/smart/i585887.jpeg"
   }
 ];
